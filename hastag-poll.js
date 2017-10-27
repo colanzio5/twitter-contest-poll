afb6974e4b7641d0bdd339ce00e972e68dfa2a23
Colin Casazza <Colanzio5@gmail.com>
2017-10-27 01:35:17 +0100

    update

--- hastag-poll.js
+++ hastag-poll.js
@@ -2,10 +2,10 @@ var Twit = require('twit');
 var fs = require('fs');
 
 var T = new Twit({
-    consumer_key: '3442mFKp54ciOmVUsA5oVOdVT',
-    consumer_secret: 'lfNpSyDrShwGTCppH9SYlC9QP3TFQdNXLw7JJUyXMbrBuiusVS',
-    access_token: '916052172927787008-JNiH0YAniHRyJ4fwzwvjAlgUwCs3HM3',
-    access_token_secret: 'ElpdbAQPucBxUOG5e24p9v0doB5zn2DwUEI1NJg0DsYqx',
+    consumer_key: '',
+    consumer_secret: '',
+    access_token: '',
+    access_token_secret: '',
 });
 
 var words = [
